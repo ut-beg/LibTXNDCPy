74269e9a54cba62a5911b83e10217c50d9d03036
Aaron Averett <aaverett@gmail.com>
2023-09-24 14:01:18 -0500

    Added a C# version, for better integration with Continuum.

---
+++ LibTXNDCPy/LibTXReSciCollCSharp/RSCSample.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibTXReSciCollCSharp
+{
+    /// <summary>
+    /// Represents a sample of some kind from an oil or gas well.  While not declared as an abstract class, this class is really meant to be extended.
+    /// </summary>
+    public class RSCSample : RSCItem
+    {
+        /// <summary>
+        /// A description of the type of sample, such as a rock core, cuttings, thin section, etc.
+        /// </summary>
+        public string? SampleType { get; set; }
+
+        protected override List<string> ComposeAbstractElements()
+        {
+            List<string> elements = base.ComposeAbstractElements();
+
+            if (SampleType != null)
+            {
+                AddAbstractElement(SampleType, "Sample Type", elements);
+            }
+
+            return elements;
+        }
+    }
+}
